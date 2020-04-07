203431673b6e908c5839e769a8006225c66577e8
Yvan Satyawan <y_satyawan@hotmail.com>
2020-04-07 16:59:20 +0200

    PEP8 Compliance

--- src/model/model.py
+++ src/model/model.py
@@ -8,9 +8,10 @@ Author:
 import torch
 import torch.nn as nn
 
+
 class FCNetwork(nn.Module):
     def __init__(self, in_connections: int, num_classes: int,
-                 first_layer: int, second_layer:int,
+                 first_layer: int, second_layer: int,
                  leaky: tuple):
         """Creates the FC network.
 
@@ -22,26 +23,25 @@ class FCNetwork(nn.Module):
         """
         super(FCNetwork, self).__init__()
         self.in_connections = in_connections
-        ReLUs = (nn.ReLU() if not leaky[0] else nn.LeakyReLU(),
+        relus = (nn.ReLU() if not leaky[0] else nn.LeakyReLU(),
                  nn.ReLU() if not leaky[1] else nn.LeakyReLU())
         self.fc0 = nn.Sequential(nn.Linear(in_connections, first_layer),
-                                 ReLUs[0])
+                                 relus[0])
         self.fc1 = nn.Sequential(nn.Linear(first_layer, second_layer),
-                                 ReLUs[1])
+                                 relus[1])
         self.fc2 = nn.Linear(second_layer, num_classes)
 
-    def forward(self, input) -> tuple:
+    def forward(self, x) -> tuple:
         """Runs forward on the network.
 
         Args:
-            input (torch.Tensor): Input as a [n, 1, 28, 28] or [n, 28, 28]
+            x (torch.Tensor): Input as a [n, 1, 28, 28] or [n, 28, 28]
                 shaped tensor.
         """
-        input = input.reshape(input.shape[0], self.in_connections)
-        x1 = self.fc0(input)
+        x = x.reshape(x.shape[0], self.in_connections)
+        x1 = self.fc0(x)
         x2 = self.fc1(x1)
         x3 = self.fc2(x2)
 
         # We return the result of every layer for visualization purposes
-
-        return (x1, x2, x3)
+        return x1, x2, x3
