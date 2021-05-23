b2540c0e1f364f0ba5a61dae146130be74cacff3
Yvan Satyawan <y_satyawan@hotmail.com>
2021-05-23 18:32:52 +0700

    Use a sine function for animation instead of a counter

--- src/physical.py
+++ src/physical.py
@@ -11,8 +11,8 @@ Created on:
 import board
 import neopixel
 import numpy as np
-from math import floor
-from time import sleep
+from math import floor, sin, pi
+from time import sleep, time
 from inference import AI
 from colors import LINEAR_TUPLE
 from argparse import ArgumentParser
@@ -20,13 +20,17 @@ from startup_sequence import startup
 from PIL import Image
 
 
-SLEEP_DURATION = 1
 PIXEL_PIN = board.D12
 NUM_PIXELS = 45
 BRIGHTNESS = 0.2
 ORDER = neopixel.RGB
 
-NN_LAYERS = [10, 15, 10]
+C_LEN = (0, 10, 15, 10, 10)  # Individual Column Lengths
+
+# Animation Parameters
+HALF_PERIOD = 2
+OFFSET = 0.2
+PEAK_DURATIONS = (0.5, 0.5, 1.5, 1.5)
 
 PIXELS = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS, brightness=BRIGHTNESS,
                            auto_write=False, pixel_order=ORDER)
@@ -45,8 +49,8 @@ def get_next_values(ai) -> (np.ndarray, int, list, list):
     """Runs inference and parses the output of the network."""
     img, out, h1, h2 = ai.infer_next()
     # Reshape and normalize the layers
-    h1 = h1.reshape(NN_LAYERS[0])
-    h2 = h2.reshape(NN_LAYERS[1])
+    h1 = h1.reshape(C_LEN[1])
+    h2 = h2.reshape(C_LEN[2])
 
     h1 *= 255. / h1.max()
     h2 *= 255. / h2.max()
@@ -57,23 +61,67 @@ def get_next_values(ai) -> (np.ndarray, int, list, list):
     return img, out, h1, h2
 
 
+def animation_running(curr_time: float, half_period=2, offset=0,
+                       peak_duration=0) -> bool:
+    """Calculates based on the given parameters if the animation is finished.
+
+    This function is given the parameters of the final column of LEDs.
+
+    Returns:
+        True if animation is not yet finished.
+    """
+    return curr_time <= (half_period + offset + peak_duration)
+
+def brightness_calc(curr_time: float, half_period=2., offset=0.,
+                    peak_duration=0.) -> float:
+    """Calculates the color value multiplier at a given time.
+
+    Args:
+        curr_time: The current time. Must be greater than 0. 0 is the starting
+            time of the current animation.
+        half_period: How long the animation lasts in ms. Defaults to 2 s
+        offset: How many s to wait after the beginning of the animation before
+            starting. Defaults to 0 s.
+        peak_duration: How long to hold the peak value in ms. Defaults to 0 s
+
+    Returns:
+        A value between 0 and 1.
+    """
+    if peak_duration == 0.:
+        # It's a simple sine function
+        return max(0, sin(((pi * curr_time) - (offset * pi)) / half_period))
+    else:
+        # Then it becomes piecewise function
+        x_0 = offset                    # Start of animation
+        x_1 = x_0 + (half_period / 2)   # End of rising part
+        x_2 = x_1 + peak_duration       # End of peak hold
+        x_3 = x_2 + (half_period / 2)   # End of animation
+
+        if x_0 <= curr_time < x_1:
+            # The first part (i.e. the rising portion is done like usual
+            return brightness_calc(curr_time, half_period, offset, 0)
+        elif x_1 <= curr_time < x_2:
+            # During the peak duration, always output 1
+            return 1.
+        elif x_2 <= curr_time < x_3:
+            # The descending part of the curve
+            return brightness_calc(curr_time, half_period,
+                                   offset + peak_duration, 0)
+        else:
+            # otherwise, stay at 0
+            return 0.
+
 def main(root, model):
     ai = AI(root, model)
 
-    playing = True
-    current_tick = 0
-    img, out, h0, h1 = get_next_values(ai)
-
+    # Initial start condition
+    playing = False
 
     while True:
         if not playing:
-            # If it's done playing the animation, do the next one after 1 second
-            sleep(SLEEP_DURATION)
+            # When done with the animation, get next values
             img, out, h0, h1 = get_next_values(ai)
-            playing = True
-        else:
-            step = int(floor(current_tick / 255))
-            t_val = current_tick % 255
+
             # Check to see if the image is an image or a numpy array
             if not isinstance(img, Image.Image):
                 # If it isn't, then turn it into one and rotate and resize it
@@ -81,38 +129,41 @@ def main(root, model):
                 img = img.resize((320, 320), Image.NEAREST)
                 img = img.rotate(90)
 
-            if step == 2:
-                # Finish layer animation, aka output layer
-                for i in range(NN_LAYERS[2]):
-                    px_val = NN_LAYERS[0] + NN_LAYERS[1] + i
-                    if i == out:
-                        color = LINEAR_TUPLE[floor(t_val)]
-                        PIXELS[px_val] = color       # Neuron
-                        PIXELS[(i + 1) * -1] = color  # Number
-                    else:
-                        PIXELS[px_val] = LINEAR_TUPLE[0]        # Neuron
-                        PIXELS[(i + 1) * -1] = LINEAR_TUPLE[0]  # Number
-            elif step < 2:
-                for i in range(NN_LAYERS[step]):
-                    if step == 0:
-                        px_val = i
-                        h = h0
-                    else:
-                        px_val = NN_LAYERS[0] + i
-                        h = h1
+            # Make h0, h1, out into a linear array of values
+            activations = [0] * 45
 
-                    PIXELS[px_val] = LINEAR_TUPLE[floor(
-                        (h[i] / 255.) * t_val
-                    )]
+            # First assign values to h0 and h1
+            activations[:C_LEN[1]] = h0
+            activations[C_LEN[1]:C_LEN[1] + C_LEN[2]] = h1
 
-            # Add to tick if still animating and actually color pixels
-            if step < 3:
-                PIXELS.show()
-                current_tick += 6
-                sleep(0.00005)
-            else:
-                current_tick = 0
-                playing = False
+            # Then assign 1 to the correct final output values
+            activations[C_LEN[1] + C_LEN[2] + out] = 255
+            activations[(out + 1) * -1] = 255
+
+            start_time = time()
+            playing = True
+        else:
+            px_vals = activations.copy()
+            curr_time = time() - start_time
+            for i in range(4):
+                # Iterate by column
+                brightness = brightness_calc(curr_time,
+                                             HALF_PERIOD,
+                                             OFFSET * i,
+                                             PEAK_DURATIONS[i])
+                px_vals[sum(C_LEN[:i + 1]):sum(C_LEN[:i + 2])] = [
+                    floor(i * brightness)
+                    for i in px_vals[sum(C_LEN[:i + 1]):sum(C_LEN[:i + 2])]
+                ]
+                # px_vals is now a value between 0 and 255
+
+            for i in range(len(px_vals)):
+                PIXELS[i]=LINEAR_TUPLE[px_vals[i]]
+
+            PIXELS.show()
+            playing = animation_running(curr_time, HALF_PERIOD, OFFSET * 3,
+                                        PEAK_DURATIONS[3])
+            sleep(0.0005)
 
 
 if __name__ == '__main__':
